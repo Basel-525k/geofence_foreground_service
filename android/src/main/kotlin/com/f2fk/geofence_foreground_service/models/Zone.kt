580e95d3e3d1b9569a45882482ca81f4ff4c1078
Adrian Verban <verbanady@gmail.com>
2024-12-10 10:48:33 +0200

    Added new fields to Zone class, in both Kotlin and Dart
    - triggers, used to pass triggers independently
    - expirationDuration, used for adding temporary geofences
    - dwellLoiteringDelay, used to add a delay for when the device is considered to loiter inside a geofence
    - initialTrigger, provide an initial trigger which is triggered if the conditions are met when adding a geofence
    - updated fromJson/toJson functions to include the new fields

--- android/src/main/kotlin/com/f2fk/geofence_foreground_service/models/Zone.kt
+++ android/src/main/kotlin/com/f2fk/geofence_foreground_service/models/Zone.kt
@@ -1,10 +1,10 @@
 package com.f2fk.geofence_foreground_service.models
 
 import com.f2fk.geofence_foreground_service.Constants
+import com.f2fk.geofence_foreground_service.utils.Utils
 import com.google.android.gms.maps.model.LatLng
 import java.io.Serializable
 import java.util.ArrayList
-
 class ZonesList(
     val zones: ArrayList<Zone>?
 ) : Serializable {
@@ -25,16 +25,10 @@ class ZonesList(
     companion object {
         fun fromJson(json: Map<String, Any>): ZonesList {
             val zones = mutableListOf<Zone>()
-
-            (json[Constants.zones] as List<Map<String, Any>>).forEach {
-                zones.add(
-                    Zone.fromJson(it)
-                )
+            (Utils.cast<List<Map<String, Any>>>(json[Constants.zones])).forEach {
+                zones.add(Zone.fromJson(it))
             }
-
-            return ZonesList(
-                zones.toCollection(ArrayList<Zone>())
-            )
+            return ZonesList(zones.toCollection(ArrayList<Zone>()))
         }
     }
 }
@@ -43,7 +37,11 @@ class Zone(
     val zoneId: String,
     val radius: Float,
     val coordinates: ArrayList<LatLng>?,
-    val notificationResponsivenessMs: Int?
+    val notificationResponsivenessMs: Int?,
+    val triggers: Int,
+    val expirationDuration: Long?,
+    val dwellLoiteringDelay: Int?,
+    val initialTrigger: Int?
 ) : Serializable {
     fun toJson(): Map<String, Any> {
         val result = mutableMapOf<String, Any>()
@@ -59,6 +57,20 @@ class Zone(
         if (notificationResponsivenessMs != null) {
             result[Constants.notificationResponsivenessMs] = notificationResponsivenessMs
         }
+        // We will convert the even triggers back to a list of integers, just as we receive them
+        // inside 'fromJson' function
+        val triggersList = mutableListOf<Int>()
+        Utils.availableTriggers.forEach { if ((it and triggers) != 0) triggersList.add(it) }
+        result[Constants.fenceTriggers] = triggersList
+        if (expirationDuration != null) {
+            result[Constants.fenceExpirationDuration] = expirationDuration
+        }
+        if (dwellLoiteringDelay != null) {
+            result[Constants.dwellLoiteringDelay] = dwellLoiteringDelay
+        }
+        if (initialTrigger != null) {
+            result[Constants.initialTrigger] = initialTrigger
+        }
 
         return result
     }
@@ -66,21 +78,23 @@ class Zone(
     companion object {
         fun fromJson(json: Map<String, Any>): Zone {
             val coordinates = mutableListOf<LatLng>()
-
-            (json[Constants.coordinates] as List<Map<String, Double>>).forEach {
-                coordinates.add(
-                    LatLng(
-                        it[Constants.latitude]!!,
-                        it[Constants.longitude]!!
-                    )
-                )
+            (Utils.cast<List<Map<String, Double>>>(json[Constants.coordinates])).forEach {
+                coordinates.add(LatLng(it[Constants.latitude]!!,it[Constants.longitude]!!))
             }
+
+            var fenceTriggers = Utils.cast<List<Int>?>(json[Constants.fenceTriggers])
+            if (fenceTriggers == null) fenceTriggers = Utils.availableTriggers
 
             return Zone(
                 json[Constants.id] as String,
                 (json[Constants.radius] as Number).toFloat(),
                 coordinates.toCollection(ArrayList<LatLng>()),
-                (json[Constants.notificationResponsivenessMs] as Number?)?.toInt()
+                (json[Constants.notificationResponsivenessMs] as Number?)?.toInt(),
+                // The event triggers are passed as a bitwise-OR operation value.
+                fenceTriggers.fold(0) { acc, trigger -> acc or trigger },
+                (json[Constants.fenceExpirationDuration] as Number?)?.toLong(),
+                (json[Constants.dwellLoiteringDelay] as Number?)?.toInt(),
+                (json[Constants.initialTrigger] as Number?)?.toInt(),
             )
         }
     }
--- lib/models/zone.dart
+++ lib/models/zone.dart
@@ -1,10 +1,16 @@
+import 'package:geofence_foreground_service/constants/geofence_event_type.dart';
 import 'package:latlng/latlng.dart';
 
 import '../constants/json_keys.dart';
 
 class Zone {
+  /// A unique identifier for the geofence zone.
   final String id;
+
+  /// The radius of the geofence zone in meters.
   final double radius;
+
+  /// List of coordinates of the geofence zone.
   final List<LatLng> coordinates;
 
   /// The millisecond delay between entering the zone and receiving the
@@ -19,46 +25,99 @@ class Zone {
   /// geofence might adjust the responsiveness value to save power when needed.
   final int? notificationResponsivenessMs;
 
+  /// The list of triggers that will be used to monitor the geofence zone.
+  ///
+  /// The default value contains all available triggers. Which are: enter, exit, and dwell.
+  ///
+  /// The current implementation for this field is only supported on Android devices.
+  ///
+  /// There is no ETA for iOS support.
+  final List<GeofenceEventType> triggers;
+
+  /// The duration after which the geofence will expire.
+  ///
+  /// If the value is not provided the geofence will have no expiration duration.
+  ///
+  /// The current implementation for this field is only supported on Android devices.
+  final Duration? expirationDuration;
+
+  /// The time it will take to trigger the dwell event after the user enters the geofence zone.
+  ///
+  /// If not provided, it means that the event should be triggered immediately.
+  ///
+  /// The current implementation for this field is only supported on Android devices.
+  final Duration? dwellLoiteringDelay;
+
+  /// The initial trigger for the geofence zone.
+  ///
+  /// If, at the moment of adding the geofence, the initial trigger matches one of the triggers,
+  /// the event will be triggered.
+  ///
+  /// For example: if the initial trigger is set to [GeofenceEventType.enter] and the user is
+  /// inside the geofence zone when the zone is added, the [GeofenceEventType.enter] event will
+  /// be triggered.
+  ///
+  /// The default value is [GeofenceEventType.enter].
+  ///
+  /// The current implementation for this field is only supported on Android devices.
+  final GeofenceEventType initialTrigger;
+
   Zone({
     required this.id,
     required this.radius,
     required this.coordinates,
     this.notificationResponsivenessMs,
+    this.triggers = const [
+      GeofenceEventType.enter,
+      GeofenceEventType.exit,
+      GeofenceEventType.dwell,
+    ],
+    this.expirationDuration,
+    this.dwellLoiteringDelay,
+    this.initialTrigger = GeofenceEventType.enter,
   });
 
-  factory Zone.fromJson(Map<String, dynamic> json) {
-    List<LatLng> jsonCoordinates = [];
+  factory Zone.fromJson(Map<String, dynamic> json) => Zone(
+        id: json[JsonKeys.id] as String,
+        radius: json[JsonKeys.radius] as double,
+        coordinates: _coordinatesFromJson(json[JsonKeys.coordinates]),
+        notificationResponsivenessMs: json[JsonKeys.notificationResponsivenessMs],
+        triggers: _triggersFromJson(json[JsonKeys.fenceTriggers]),
+        expirationDuration: json[JsonKeys.fenceExpirationDuration] != null
+            ? Duration(milliseconds: json[JsonKeys.fenceExpirationDuration] as int)
+            : null,
+        dwellLoiteringDelay: json[JsonKeys.dwellLoiteringDelay] != null
+            ? Duration(milliseconds: json[JsonKeys.dwellLoiteringDelay] as int)
+            : null,
+        initialTrigger: GeofenceEventType.findById(json[JsonKeys.initialTrigger] as int?),
+      );
 
-    if (json[JsonKeys.coordinates] != null) {
-      List coordinatesJsonList = json[JsonKeys.coordinates] as List;
-
-      for (Map<String, double> coordinates in coordinatesJsonList) {
-        jsonCoordinates.add(LatLng.degree(
-          coordinates[JsonKeys.latitude]!,
-          coordinates[JsonKeys.longitude]!,
-        ));
-      }
-    }
-
-    return Zone(
-      id: json[JsonKeys.id],
-      radius: json[JsonKeys.radius],
-      coordinates: jsonCoordinates,
-      notificationResponsivenessMs: json[JsonKeys.notificationResponsivenessMs],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      JsonKeys.id: id,
-      JsonKeys.radius: radius,
-      JsonKeys.coordinates: coordinates
-          .map((e) => {
-                JsonKeys.latitude: e.latitude.degrees,
-                JsonKeys.longitude: e.longitude.degrees,
-              })
-          .toList(),
-      JsonKeys.notificationResponsivenessMs: notificationResponsivenessMs,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        JsonKeys.id: id,
+        JsonKeys.radius: radius,
+        JsonKeys.coordinates: coordinates
+            .map((e) => {
+                  JsonKeys.latitude: e.latitude.degrees,
+                  JsonKeys.longitude: e.longitude.degrees,
+                })
+            .toList(),
+        JsonKeys.notificationResponsivenessMs: notificationResponsivenessMs,
+        JsonKeys.fenceTriggers: triggers.map((e) => e.value).toList(),
+        JsonKeys.fenceExpirationDuration: expirationDuration?.inMilliseconds,
+        JsonKeys.dwellLoiteringDelay: dwellLoiteringDelay?.inMilliseconds,
+        JsonKeys.initialTrigger: initialTrigger.value,
+      };
 }
+
+List<LatLng> _coordinatesFromJson(List<dynamic>? coordinatesJsonList) => List.generate(
+      coordinatesJsonList?.length ?? 0,
+      (index) {
+        final coordinates = coordinatesJsonList![index] as Map<String, double>;
+        return LatLng.degree(coordinates[JsonKeys.latitude]!, coordinates[JsonKeys.longitude]!);
+      },
+    );
+
+List<GeofenceEventType> _triggersFromJson(List<dynamic>? triggersJsonList) => List.generate(
+      triggersJsonList?.length ?? 0,
+      (index) => GeofenceEventType.findById(triggersJsonList![index] as int?),
+    );
